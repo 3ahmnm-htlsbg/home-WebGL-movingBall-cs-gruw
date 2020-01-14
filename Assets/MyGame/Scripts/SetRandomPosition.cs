c9c32d9737832ce985e9fc5184c938600c59ee6c
Will <wilfried.gruber@hotmail.com>
2020-01-14 11:02:41 +0100

    Add Scripts Folder with SetRandomPosition Script

---
+++ Assets/MyGame/Scripts/SetRandomPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRandomPosition : MonoBehaviour
+{
+    public GameObject[] ballPositions;
+    public GameObject ball;
+
+    public void SetNewPosition()
+    {
+        int randomValue = Random.Range(0,4);
+        Vector3 position = ballPositions[randomValue].transform.position;
+
+        ball.transform.position = position;
+    }
+    
+}
